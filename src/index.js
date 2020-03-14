231e12d133f60a44e84dc89f976b41c1dea11d1a
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-03-14 14:13:48 -0300

    Loading Images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = "/dist/" + Icon;
+    element.appendChild(myIcon);
+
+    console.log('teste');
 
     return element;
 }
